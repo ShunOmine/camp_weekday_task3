1a33c3b3f932ead84462eaaff0ea3b0d5e7547cc
ShunOmine <shun12baseball@gmail.com>
2019-03-05 13:55:04 +0900

    PR ~end fixed calcs_controller add show.html.erb

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,22 +1,16 @@
 class CalcsController < ApplicationController
-  def addition
+  def show
     @number_one = params[:number_one].to_i
     @number_two = params[:number_two].to_i
-    @answer = @number_one + @number_two
-  end
-  def subtraction
-    @number_one = params[:number_one].to_i
-    @number_two = params[:number_two].to_i
-    @answer = @number_one - @number_two
-  end
-  def multiplication
-    @number_one = params[:number_one].to_i
-    @number_two = params[:number_two].to_i
-    @answer = @number_one * @number_two
-  end
-  def division
-    @number_one = params[:number_one].to_i
-    @number_two = params[:number_two].to_i
-    @answer = @number_one / @number_two
+    @calculation = params[:calculation]
+    if @calculation == "addition"
+      @answer = @number_one + @number_two
+    elsif @calculation == "subtraction"
+      @answer = @number_one - @number_two
+    elsif @calculation == "multiplication"
+        @answer = @number_one * @number_two
+    elsif @calculation == "division"
+      @answer = @number_one / @number_two
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'calcs/:number_one/addition/:number_two' => 'calcs#addition'
-  get 'calcs/:number_one/subtraction/:number_two' => 'calcs#subtraction'
-  get 'calcs/:number_one/multiplication/:number_two' => 'calcs#multiplication'
-  get 'calcs/:number_one/division/:number_two' => 'calcs#division'
+  get 'calc/:number_one/:calculation/:number_two' => 'calcs#show'
 end
