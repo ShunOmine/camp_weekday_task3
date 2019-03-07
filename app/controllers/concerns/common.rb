d45c558322957a67ae016d3ea8f9a55aa16f223e
ShunOmine <shun12baseball@gmail.com>
2019-03-07 18:43:34 +0900

    day4 exercisec ~edit common.rb

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -3,24 +3,24 @@ module Common
     @number_one = params[:number_one].to_i
     @number_two = params[:number_two].to_i
     @calculation = params[:calculation]
-    if @calculation == "addition"
-      @answer = @number_one + @number_two
-    elsif @calculation == "subtraction"
-      @answer = @number_one - @number_two
-    elsif @calculation == "multiplication"
-      @answer = @number_one * @number_two
-    elsif @calculation == "division"
-      if @number_one >= @number_two
-        begin
-          @answer = @number_one / @number_two
-        rescue ZeroDivisionError
-          @answer = "divided by 0"
-        end
-      else
-        @division_one = @number_one / @number_two
-        @division_two = @number_one % @number_two
-        @answer = "#{@division_one}..#{@division_two}"
-      end
-    end
+    @answer = if @calculation == "addition"
+                @number_one + @number_two
+              elsif @calculation == "subtraction"
+                @number_one - @number_two
+              elsif @calculation == "multiplication"
+                @number_one * @number_two
+              elsif @calculation == "division"
+                if @number_one >= @number_two
+                  begin
+                    @number_one / @number_two
+                  rescue ZeroDivisionError
+                    "divided by 0"
+                  end
+                else
+                  @division_one = @number_one / @number_two
+                  @division_two = @number_one % @number_two
+                  "#{@division_one}..#{@division_two}"
+                end
+              end
   end
 end
