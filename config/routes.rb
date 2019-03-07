4944af1204bc3cad0d5e05abcfc0271710e0460d
ShunOmine <shun12baseball@gmail.com>
2019-03-07 12:08:45 +0900

    day4 exercise ~edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'calc/:number_one/:calculation/:number_two' => 'calcs#show'
+  get 'calcs/:number_one/:calculation/:number_two' => 'calcs#show'
 end
